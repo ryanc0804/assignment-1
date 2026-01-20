72ad6e83ddf55b9132730e3e319669d09120b6b7
ryanc0804 <ryanc0804@users.noreply.github.com>
2026-01-20 17:10:01 -0500

    Fix type annotations in fibRoute.ts

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -1,15 +1,17 @@
 // Endpoint for querying the fibonacci numbers
 
+import { Request, Response } from "express";
 import fibonacci from "./fib";
 
-export default (req, res) => {
+export default (req: Request, res: Response) => {
   const { num } = req.params;
+  const numStr = num as string;
 
-  const fibN = fibonacci(parseInt(num));
-  let result = `fibonacci(${num}) is ${fibN}`;
+  const fibN = fibonacci(parseInt(numStr));
+  let result = `fibonacci(${numStr}) is ${fibN}`;
 
   if (fibN < 0) {
-    result = `fibonacci(${num}) is undefined`;
+    result = `fibonacci(${numStr}) is undefined`;
   }
 
   res.send(result);
